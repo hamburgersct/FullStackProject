46be1b64e4f931adc7d9f875fe9d3f7f59537d6f
hamburgersct <hamburgersct@gmail.com>
2021-08-16 19:51:34 +0800

    first commit

---
+++ frontend/todo-app/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import FirstComponent from './components/learning-example/FirstComponent';
+import SecondComponent from './components/learning-example/SecondComponent';
+import ThirdComponent from './components/learning-example/ThirdComponent';
+import Counter from './components/counter/Counter';
+import './App.css';
+ 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Counter />
+      </div>
+    );
+  }
+}
+
+class LearningComponent extends Component {
+  render() {
+    return (
+      <div className="LearningComponent">
+        My Hello World
+        <FirstComponent />
+        <SecondComponent />
+        <ThirdComponent />
+      </div>
+    );
+  }
+}
+
+
+export default App;
