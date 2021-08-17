57f08900f31042a84c2e1cf79421a5cd14101f20
hamburgersct <hamburgersct@gmail.com>
2021-08-17 17:20:27 +0800

    remove unnecessary methods

--- frontend/todo-app/src/components/counter/CounterButton.jsx
+++ frontend/todo-app/src/components/counter/CounterButton.jsx
@@ -5,11 +5,6 @@ import './CounterButton.css'
 class CounterButton extends Component {
     constructor() {
         super()
-        this.state = {
-            counter : 0
-        }
-        this.increment = this.increment.bind(this)
-        this.decrement = this.decrement.bind(this)
     }
 
     // we can change original function into arrow function
@@ -18,34 +13,14 @@ class CounterButton extends Component {
         // const style = {fontSize: "35px", padding: "15px 30px"}
         return (
             <div className="counter">
-            <button onClick={this.increment}>+{this.props.by}</button>
-            <button onClick={this.decrement}>-{this.props.by}</button>
+            <button onClick={() => this.props.incrementMethod(this.props.by)}>+{this.props.by}</button>
+            <button onClick={() => this.props.decrementMethod(this.props.by)}>-{this.props.by}</button>
             {/*<span className="count" 
             //  style={style}
             >{this.state.counter}
             </span>*/}
             </div>
         )
-    }
-
-    increment() {
-        this.setState(
-            (prevState) => {
-            return {counter : prevState.counter + this.props.by}
-            }
-        )
-        // to call the increment() in Counter
-        this.props.incrementMethod(this.props.by)
-    }
-
-    decrement() {
-        this.setState(
-            (prevState) => {
-            return {counter : prevState.counter - this.props.by}
-            }
-        )
-        // to call the decrement() in Counter
-        this.props.decrementMethod(this.props.by)
     }
 }
 
