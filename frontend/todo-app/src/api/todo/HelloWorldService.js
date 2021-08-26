8b3d0f146431a2c67046817e0ff1e886efd9e26a
hamburgersct <hamburgersct@gmail.com>
2021-08-26 13:12:37 +0800

    add HTTP requests of different types

--- frontend/todo-app/src/api/todo/HelloWorldService.js
+++ frontend/todo-app/src/api/todo/HelloWorldService.js
@@ -5,6 +5,14 @@ class HelloWorldService {
         // Here returns an Promise, in the method who calls it, we can define then() to execute other operations to execute
         return axios.get("http://localhost:8080/hello-world")
     }
+
+    executeHelloWorldBeanService() {
+        return axios.get("http://localhost:8080/hello-world-bean")
+    }
+
+    executeHelloWorldPathVariableService(name) {
+        return axios.get(`http://localhost:8080/hello-world/path-variable/${name}`)
+    }
 }
 
 export default new HelloWorldService()
