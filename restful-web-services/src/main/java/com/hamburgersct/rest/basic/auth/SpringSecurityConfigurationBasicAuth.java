74ae5723fe7057369a8f8e5933119550592f54b0
hamburgersct <hamburgersct@gmail.com>
2021-09-18 00:04:39 -0700

    add JWT configurations

--- restful-web-services/src/main/java/com/hamburgersct/rest/basic/auth/SpringSecurityConfigurationBasicAuth.java
+++ restful-web-services/src/main/java/com/hamburgersct/rest/basic/auth/SpringSecurityConfigurationBasicAuth.java
@@ -1,4 +1,4 @@
-package com.hamburgersct.rest.webservices.restfulwebservices.basic.auth;
+package com.hamburgersct.rest.basic.auth;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
